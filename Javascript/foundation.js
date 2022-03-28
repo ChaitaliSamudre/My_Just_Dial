7ff0177b8ccca30eba0bce7009df688008aeb87a
Chaitali Samudre <chaitali_samudre@thbs.india.com>
2022-03-28 09:11:19 +0530

    added javascript variable

---
+++ Javascript/foundation.js
@@ -0,0 +1,20 @@
+let batchNumber = 99; //number datatype
+console.log(batchNumber);
+console.log(typeof batchNumber);
+
+let specilization = 'Mean/Mern';
+console.log(specilization);
+console.log(typeof specilization);
+
+//isLegacy = 'Yes';
+//conole.log(isLegacy);
+//console.log(typeof isLegacy);
+
+let fname = 'Chaitali';
+let lname = 'Samudre';
+ffname = fname + lname; //concatination
+
+console.log(ffname);
+console.log(typeof ffname);
+
+
---
+++ Javascript/function.js
@@ -0,0 +1,10 @@
+function addType(avar1, avar2){
+    return avar1 + avar2;
+}
+addType('Chaitali','Samudre');
+
+let fname = 'Chaitali';
+let lname = 'Samudre';
+
+console.log(addType('Chaitali', 'Samudre'));
+console.log(addType('Chaitali',20));
