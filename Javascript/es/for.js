68f8d40ed57ec1979d4cdd8f1ae209b7fb773a0f
Chaitali Samudre <chaitali_samudre@thbs.india.com>
2022-03-30 16:39:01 +0530

    added for loop methods

--- Javascript/es/for.js
+++ Javascript/es/for.js
@@ -41,6 +41,47 @@ let filteredPerson = personList.filter((val, indx, arr) => {
 console.log(`=====Filtered info=======`)
 console.log(filteredPerson);
 
+//find going to loop as soon as condition becomes true
+const arr1 = [1,2,5,6,7,8,50,89];
+const found = arr1.find((element,index) => {
+    console.log(element,index)
+    return element > 5;
+});
+console.log(found);
+
+//findIndex method
+const arr2 = [1,200,5,100,7,8,50,89];
+const found1 = arr2.findIndex((element,index) => {
+    console.log(element,index)
+    return element > 200;
+});
+console.log(`condition satified element index is --> ${found1}`);
+
+//indexof and join 
+
+const ind=[19,34,5,6,7,8];
+console.log(`=======Index of========`);
+console.log(ind.indexOf(6));
+
+console.log(`=======Joining array========`);
+console.log(ind.join('-'));
+
+
+// MAP is used to transform or modifed array
+
+let personMap = [
+    {name : 'Chaitali' , skill: ['java', 'html', 'css']},
+    {name : 'Priya' , skill: ['java', 'html', 'css', 'javascript']},
+    {name : 'Sujit' , skill: ['java', 'html', 'css','python']}
+];
+
+let mappedPerson = personMap.map((val, inx, arr) => {
+    val.name = "Hello " + val.name; 
+    return val;
+});
+console.log(mappedPerson);
+
+
 
    
 
